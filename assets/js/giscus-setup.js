606f27b832a244cc312a15b2d20b23339c67f917
MelodiousPotato <87632596+MelodiousPotato@users.noreply.github.com>
2026-03-28 02:32:26 +0000

    Deploying to gh-pages from @ MelodiousPotato/MelodiousPotato.github.io@1c181486206c232b0ad85838f0c7b93b4761e177 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "MelodiousPotato/Rebekah_Mou_Portfolio",
+    "data-repo": "MelodiousPotato/MelodiousPotato.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
